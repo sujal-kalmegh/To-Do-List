9669e767ad2ea31b549b9c5bb6993e8180c98238
SKalmegh39 <sujalkalmegh23@gmail.com>
2025-08-30 23:35:11 +0530

    V3

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+function clearAll() {
+  document.getElementById("task-list").innerHTML = ""
+}
+
+document.getElementById("task-Input").addEventListener("keypress", function(event){
+  if(event.key === "Enter"){
+    addTask()
+  }
+})
+
 let tasks = []
 let status = []
 function addTask() {
@@ -9,14 +19,26 @@ function addTask() {
   }
   // tasks.push(taskInput)
   // console.log(tasks)
-
   const li = document.createElement("li")
-  li.textContent = taskInput
 
-  li.onclick = () => {
+  const span = document.createElement("span")
+  span.textContent = taskInput
+  span.style.cursor = "pointer"
+
+  // li.textContent = taskInput
+
+  span.onclick = () => {
     li.classList.toggle("completed")
-    
   }
+
+  const delBtn = document.createElement("button")
+  delBtn.textContent = "❌"
+  delBtn.onclick = () => {
+    li.remove()
+  }
+
+  li.appendChild(span)
+  li.appendChild(delBtn)
 
   document.getElementById("task-list").appendChild(li)
 
